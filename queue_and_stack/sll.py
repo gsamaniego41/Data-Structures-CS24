5507a8a3d3f83a85577c825d56871b0435634d05
Gabriel Samaniego <selfdev.gs@gmail.com>
2019-12-17 22:13:14 -0800

    lru_cache still worki n progress

---
+++ queue_and_stack/sll.py
@@ -0,0 +1,14 @@
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.next = next
+
+
+class SLL:
+    def __init__(self, head=None, tail=None):
+        self.head = head
+        self.tail = tail
+        self.length = 0
+
+    def reverse():
+        #
