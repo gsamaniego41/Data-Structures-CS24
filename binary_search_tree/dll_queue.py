233dac99c08fb880ee8b66aa56779217869e8421
Gabriel Samaniego <selfdev.gs@gmail.com>
2019-12-18 16:11:45 -0800

    binary_search_tree

---
+++ binary_search_tree/dll_queue.py
@@ -0,0 +1,27 @@
+from doubly_linked_list import DoublyLinkedList
+import sys
+sys.path.append('../doubly_linked_list')
+
+
+class Queue:
+    def __init__(self):
+        self.size = 0
+        # FIFO
+        # Why is our DLL a good choice to store our elements?
+        # Bec it has buit-in methods that allow us to
+        # easily remove from the head and add to the tail
+        self.storage = DoublyLinkedList()
+
+    def enqueue(self, value):
+        self.storage.add_to_tail(value)
+        self.size += 1
+
+    def dequeue(self):
+        if self.size <= 0:
+            return None
+        else:
+            self.size -= 1
+            return self.storage.remove_from_head()
+
+    def len(self):
+        return self.size
